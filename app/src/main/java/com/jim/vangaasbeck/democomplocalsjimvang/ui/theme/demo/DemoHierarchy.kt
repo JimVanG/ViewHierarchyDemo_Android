99fd25db6d9a5993997363136974665fce021265
JimVanG <jjvg@me.com>
2025-11-24 15:55:59 -0600

    Add simple Preview to DemoHierarchy.

--- app/src/main/java/com/jim/vangaasbeck/democomplocalsjimvang/ui/theme/demo/DemoHierarchy.kt
+++ app/src/main/java/com/jim/vangaasbeck/democomplocalsjimvang/ui/theme/demo/DemoHierarchy.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -328,4 +329,14 @@ fun DemoHierarchyScreen() {
             }
         }
     }
+}
+
+@Preview(
+    name = "Demo Hierarchy Screen",
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+private fun PreviewDemoHierarchyScreen() {
+    DemoHierarchyScreen()
 }
